b091bef858698c9cff2aa7b4b178af0ee4089041
Cynny <c.dafinone@gmail.com>
2023-02-23 13:02:09 +0000

    updated cors permissions

--- index.js
+++ index.js
@@ -37,7 +37,10 @@ const app = express()
 //app.use(express.json())
 //app.use(express.urlencoded({extended: true}))
 app.use(bodyParser.json())
-app.use(cors())
+app.use(cors({
+    origin : "*", // (Whatever your frontend url is) 
+    credentials: true, // <= Accept credentials (cookies) sent by the client
+  }))
 const port = process.env.PORT || 8080
 
 /*
